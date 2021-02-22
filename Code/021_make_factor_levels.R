276e0a8a516e9683aaa6fed96f1cbc36f87d9bf2
Michael Boerman <61358854+michaelboerman@users.noreply.github.com>
2021-02-22 14:45:15 -0500

    tidy with stylr, changes to factor order again

--- Code/021_make_factor_levels.R
+++ Code/021_make_factor_levels.R
@@ -1,12 +1,12 @@
 # 021_make_factor_levels.R
 # Michael Boerman Jan 2021
-# 
+#
 # Turn the categorical variables into numbers
 # This is a data-processing script.
-# 
+#
 # output: cat_data_reordered.csv
 #         summary_df.csv
-# 
+#
 # ---- Setup ------------------------------------------------------------------#
 
 library(readr)
@@ -24,7 +24,7 @@ options(dplyr.summarise.inform = FALSE) # https://www.tidyverse.org/blog/2020/05
 data_lockdown_dummies <- read_csv(
   here("Intermediate_Data/data_lockdown_dummies.csv"),
   col_types = cols(
-    .default = col_factor(),     # coerce to be factors instead of chr
+    .default = col_factor(), # coerce to be factors instead of chr
     Date = col_date(format = "") # except column Date, read as date.
   )
 )
@@ -37,60 +37,60 @@ data_lockdown_dummies <- read_csv(
 
 cat_data_reordered <- data_lockdown_dummies %>%
   # reorder and then assign values.
-  
+
   # column 3
   mutate(Stay_Home_Order = factor(Stay_Home_Order,
-                                  levels = c("-", "Lifted", "High-Risk Groups", "Curfew", "Statewide"),
-                                  labels = c(0, 1, 2, 3, 4)
+    levels = c("-", "Lifted", "High-Risk Groups", "Curfew", "Statewide"),
+    labels = c(0, 1, 2, 3, 4)
   )) %>%
-  
-  
+
+
   # column 4.
   mutate(Status_of_Reopening = factor(Status_of_Reopening,
-                                      levels = c("Reopened", "Paused", "Closing"),
-                                      labels = c(1, 2, 3)
+    levels = c("Reopened", "Paused", "Closing"),
+    labels = c(1, 2, 3)
   )) %>%
-  
+
   # column 5.
   mutate(NonEss_Business_Closed = factor(NonEss_Business_Closed,
-                                         levels = c("-", "Other", "Some Reopened", "Reduced Capacity", "Some Closed", "All Non-Essential Retail Businesses Closed", "All Non-Essential Businesses Closed"),
-                                         labels = c(0, 0, 1, 2, 3, 4, 5)
+    levels = c("-", "Other", "Some Reopened", "Reduced Capacity", "Some Closed", "All Non-Essential Retail Businesses Closed", "All Non-Essential Businesses Closed"),
+    labels = c(0, 0, 1, 2, 3, 4, 5)
   )) %>%
-  
+
   # column 6.
   mutate(Gathering_Limit = factor(Gathering_Limit,
-                                  levels = c("-", "Other", "No Limit", "50", "25", "20", "10", "5", "All Prohbited"),
-                                  labels = c(0, 0, 1, 2, 3, 4, 5, 6, 7)
+    levels = c("-", "Other", "No Limit", "50", "25", "20", "10", "5", "All Prohbited"),
+    labels = c(0, 0, 1, 2, 3, 4, 5, 6, 7)
   )) %>%
-  
+
   # column 7. Only one factor level present.
   mutate(Emergency_Declaration = factor(Emergency_Declaration,
-                                        levels = "Yes",
-                                        labels = c(1)
+    levels = "Yes",
+    labels = c(1)
   )) %>%
-  
+
   # column 8.
   mutate(Travel_Quarantine = factor(Travel_Quarantine,
-                                    levels = c("-", "Other", "No Restrictions", "International", "Air Only", "Certain States", "All Locations"),
-                                    labels = c(0, 0, 1, 2, 3, 4, 5)
+    levels = c("-", "Other", "No Restrictions", "International", "Air Only", "Certain States", "All Travelers"),
+    labels = c(0, 0, 1, 2, 3, 4, 5)
   )) %>%
-  
+
   # column 9.
   mutate(Mask_Requirement = factor(Mask_Requirement,
-                                   levels = c("-", "No", "Certain Employees Only", "General Public", "Yes"),
-                                   labels = c(0, 1, 2, 3, 3)
+    levels = c("-", "No", "Certain Employees Only", "General Public", "Yes"),
+    labels = c(0, 1, 2, 3, 3)
   )) %>%
-  
+
   # column 10.
   mutate(Restaurants = factor(Restaurants,
-                              levels = c("-", "No Limits", "Limited Indoor", "No Indoor"),
-                              labels = c(0, 1, 2, 3)
+    levels = c("-", "No Limits", "Limited Indoor", "No Indoor"),
+    labels = c(0, 1, 2, 3)
   )) %>%
-  
+
   # column 11.
   mutate(School_Closed = factor(School_Closed,
-                                levels = c("-", "Reccomended", "Closed"),
-                                labels = c(0, 1, 2)
+    levels = c("-", "Reccomended", "Closed"),
+    labels = c(0, 1, 2)
   )) %>%
   identity()
 
@@ -117,5 +117,5 @@ summary_df <- cat_data_reordered %>%
     School_Closed = sum(School_Closed, na.rm = T)
   ) %>%
   ungroup() %>%
-  pivot_longer(cols = 2:10, names_to = "Category", values_to = "sums") %>% 
+  pivot_longer(cols = 2:10, names_to = "Category", values_to = "sums") %>%
   write_csv(here("Intermediate_Data", "summary_df.csv"))
--- Code/041_pca.R
+++ Code/041_pca.R
@@ -12,51 +12,50 @@ library(here)
 # for nice pc plots
 library(factoextra)
 
-# cat_data <- read_csv(here("Results/csv/pop_std_scores_states.csv")) %>% 
-#   pivot_wider(names_from = Category,
-#               values_from = weighted_standardized_score) %>% 
-#   select(-c(population, scores, standardized_score)) %>% 
-#   select(-c(School_Closed)) %>% 
+# 
+# cat_data <- read_csv(here("Intermediate_Data/cat_data_reordered.csv"),
+#   col_types = cols(
+#     .defulat = col_double(),
+#     Mask_Requirement = col_integer()
+#   )
+# ) %>%
+#   select(-c(School_Closed)) %>%
+#   select(-c(Status_of_Reopening)) %>%
 #   identity()
 
-cat_data <- read_csv(here("Intermediate_Data/cat_data_reordered.csv"),
-                     col_types = cols(.defulat = col_double(),
-                                      Masl_Requirement = col_integer())) %>% 
-  select(-c(School_Closed)) %>%
-  select(-c(Status_of_Reopening)) %>% 
-  identity()
-  
-cat_data <- cat_data_reordered %>% 
+cat_data <- cat_data_reordered %>%
   select(-c(Status_of_Reopening, Emergency_Declaration, School_Closed))
 
-pca_results <- prcomp(~., data = select_if(cat_data, is.numeric),
-                                 na.action = na.omit,
-                                 scale = TRUE
-                      )
+pca_results <- prcomp(~.,
+  data = select_if(cat_data, is.numeric),
+  na.action = na.omit,
+  scale = TRUE
+)
 
 
-fviz_eig(pca_results)     
+fviz_eig(pca_results)
 fviz_pca_var(pca_results, col.var = "contrib")
 get_eigenvalue(pca_results)
 
 
 pca_var <- get_pca_var(pca_results)
-pca_var$coord          # Coordinates
-pca_var$contrib        # Contributions to the PCs
-pca_var$cos2           # Quality of representation 
+pca_var$coord # Coordinates
+pca_var$contrib # Contributions to the PCs
+pca_var$cos2 # Quality of representation
 
 # Results for individuals
 pca_ind <- get_pca_ind(pca_results)
-pca_ind$coord          # Coordinates
-head(pca_ind$contrib)  # Contributions to the PCs
-pca_ind$cos2           # Quality of representation 
+pca_ind$coord # Coordinates
+head(pca_ind$contrib) # Contributions to the PCs
+pca_ind$cos2 # Quality of representation
 
-first_pc <- pca_results$x[,1] %>%
-  as.data.frame() %>% 
-  dplyr::rename(first_pc = '.') %>% 
-  mutate(period = seq(1:length(pca_results$x[,1]))) %>% 
+first_pc <- pca_results$x[, 1] %>%
+  as.data.frame() %>%
+  dplyr::rename(first_pc = ".") %>%
+  mutate(period = seq(1:length(pca_results$x[, 1]))) %>%
   identity()
 
-first_pc %>% 
-  ggplot(aes(x=period, y = first_pc)) +
-  geom_bar(stat = "identity")
+first_pc %>%
+  ggplot(aes(x = period, y = first_pc)) +
+  geom_bar(stat = "identity") +
+  ggsave(here("Results/pca", "national_index.png"))
